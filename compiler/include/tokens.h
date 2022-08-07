48a14952a8dfce81bad27ccf9cf599a76b4e9b89
Thinking-Dragon <cfmgp@hotmail.com>
2022-08-06 23:02:14 -0400

    Add header with all tokens included

---
+++ compiler/include/tokens.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "RoverScriptCompiler/token/token.h"
+
+#include "RoverScriptCompiler/token/action_stack_token.h"
+
+#include "RoverScriptCompiler/token/strategy_token.h"
+#include "RoverScriptCompiler/token/action_token.h"
+#include "RoverScriptCompiler/token/trigger_token.h"
+#include "RoverScriptCompiler/token/model_token.h"
+#include "RoverScriptCompiler/token/enum_token.h"
+
+#include "RoverScriptCompiler/token/start_token.h"
+#include "RoverScriptCompiler/token/loop_token.h"
+#include "RoverScriptCompiler/token/data_token.h"
+
+#include "RoverScriptCompiler/token/triggers_token.h"
+
+#include "RoverScriptCompiler/token/t_int_token.h"
+#include "RoverScriptCompiler/token/t_long_token.h"
+#include "RoverScriptCompiler/token/t_float_token.h"
+#include "RoverScriptCompiler/token/t_double_token.h"
+#include "RoverScriptCompiler/token/t_bool_token.h"
+#include "RoverScriptCompiler/token/t_string_token.h"
+#include "RoverScriptCompiler/token/t_timer_token.h"
+
+#include "RoverScriptCompiler/token/l_number_token.h"
+#include "RoverScriptCompiler/token/l_bool_token.h"
+#include "RoverScriptCompiler/token/l_string_token.h"
+
+#include "RoverScriptCompiler/token/if_token.h"
+#include "RoverScriptCompiler/token/else_token.h"
+#include "RoverScriptCompiler/token/do_token.h"
+#include "RoverScriptCompiler/token/while_token.h"
+#include "RoverScriptCompiler/token/for_token.h"
+
+#include "RoverScriptCompiler/token/symbol_token.h"
+
+#include "RoverScriptCompiler/token/equals_token.h"
+#include "RoverScriptCompiler/token/greater_token.h"
+#include "RoverScriptCompiler/token/less_token.h"
+#include "RoverScriptCompiler/token/greater_eq_token.h"
+#include "RoverScriptCompiler/token/less_eq_token.h"
+
+#include "RoverScriptCompiler/token/assign_token.h"
+#include "RoverScriptCompiler/token/arrow_token.h"
+
+#include "RoverScriptCompiler/token/and_token.h"
+#include "RoverScriptCompiler/token/or_token.h"
+#include "RoverScriptCompiler/token/xor_token.h"
+#include "RoverScriptCompiler/token/not_token.h"
+
+#include "RoverScriptCompiler/token/l_shift_token.h"
+#include "RoverScriptCompiler/token/r_shift_token.h"
+#include "RoverScriptCompiler/token/bit_and_token.h"
+#include "RoverScriptCompiler/token/bit_or_token.h"
+
+#include "RoverScriptCompiler/token/comment_token.h"
+
+#include "RoverScriptCompiler/token/add_token.h"
+#include "RoverScriptCompiler/token/sub_token.h"
+#include "RoverScriptCompiler/token/mul_token.h"
+#include "RoverScriptCompiler/token/div_token.h"
+#include "RoverScriptCompiler/token/mod_token.h"
+
+#include "RoverScriptCompiler/token/dot_token.h"
+#include "RoverScriptCompiler/token/comma_token.h"
+
+#include "RoverScriptCompiler/token/l_paren_token.h"
+#include "RoverScriptCompiler/token/r_paren_token.h"
+
+#include "RoverScriptCompiler/token/l_brace_token.h"
+#include "RoverScriptCompiler/token/r_brace_token.h"

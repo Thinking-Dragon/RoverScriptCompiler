72d06955df0ac6827d53c90dba239aa76276ba04
Thinking-Dragon <cfmgp@hotmail.com>
2022-08-06 23:51:22 -0400

    Add header with all ast nodes included

---
+++ compiler/include/RoverScriptCompiler/ast/ast_nodes.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "RoverScriptCompiler/ast/ast_node.h"
+
+#include "RoverScriptCompiler/ast/ast_document.h"
+#include "RoverScriptCompiler/ast/ast_strategy.h"
+#include "RoverScriptCompiler/ast/ast_action.h"
+#include "RoverScriptCompiler/ast/ast_trigger.h"
+#include "RoverScriptCompiler/ast/ast_model.h"
+#include "RoverScriptCompiler/ast/ast_enum.h"
+
+#include "RoverScriptCompiler/ast/ast_var_declaration.h"
+
+#include "RoverScriptCompiler/ast/ast_strategy_body.h"
+
+#include "RoverScriptCompiler/ast/ast_loop.h"
+#include "RoverScriptCompiler/ast/ast_start.h"
+#include "RoverScriptCompiler/ast/ast_triggers.h"
+#include "RoverScriptCompiler/ast/ast_data.h"
+
+#include "RoverScriptCompiler/ast/ast_scope.h"
+
+// #include "RoverScriptCompiler/ast/ast_trigger_map.h"
+// #include "RoverScriptCompiler/ast/ast_trigger_pair.h"
+
+// #include "RoverScriptCompiler/ast/ast_data_map.h"
+// #include "RoverScriptCompiler/ast/ast_symbol_definition.h"
+
+#include "RoverScriptCompiler/ast/ast_type.h"
+#include "RoverScriptCompiler/ast/ast_custom_type.h"
+
+#include "RoverScriptCompiler/ast/ast_symbol.h"
